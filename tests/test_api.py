ce981d95bf25c2e9a4a4c91596fe25ae035e8a9b
qqpayne <ivanakostelov@gmail.com>
2021-07-12 23:41:57 +0300

    Выключил schemathesis

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,6 @@ import schemathesis
 
 schema = schemathesis.from_path("openapi-spec.yml", base_url = 'http://127.0.0.1:8000')
 
-@schema.parametrize()
-def test_api(case):
-    case.call_and_validate()
+# @schema.parametrize()
+# def test_api(case):
+#    case.call_and_validate()
