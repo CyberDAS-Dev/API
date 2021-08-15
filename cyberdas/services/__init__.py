f5d840344c983f780d0e63450a1fe196c933a958
qqpayne <ivanakostelov@gmail.com>
2021-08-15 17:34:19 +0300

    Удалил pass_checker

--- cyberdas/services/__init__.py
+++ cyberdas/services/__init__.py
@@ -1,5 +1,4 @@
 from .mail import Mail, SignupMail
-from .pass_checker import PassChecker
 from .session import SessionManager
 
-__all__ = ['Mail', 'SignupMail', 'PassChecker', 'SessionManager']
+__all__ = ['Mail', 'SignupMail', 'SessionManager']
