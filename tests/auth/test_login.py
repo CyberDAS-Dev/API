16caf47c84b06fcb46e99d40a75e2f7fbed4b810
qqpayne <ivanakostelov@gmail.com>
2021-08-29 01:13:41 +0300

    Написал тесты на систему авторизации

--- tests/auth/test_login.py
+++ tests/auth/test_login.py
@@ -1,49 +1,110 @@
 import pytest
+from unittest.mock import ANY, MagicMock, patch
+from os import environ
 
 import falcon
 
-from conftest import (
-    REGISTERED_USER_EMAIL,
-    REGISTERED_USER_PASS,
-    SES_LENGTH,
-)
 from cyberdas.models import Session
+from cyberdas.services import TransactionMail
+from cyberdas.config import get_cfg
+
+cfg = get_cfg()
+
+USER_EMAIL = 'haha@mail.com'
+FACULTY_NAME = environ['FACULTY_NAME']
+REGISTERED_USER_EMAIL = environ['REGISTERED_USER_EMAIL']
+SES_LENGTH = int(get_cfg()['internal']['session.length'])
 
 
-class TestLogin:
+class TestSender:
 
     URI = '/login'
+    smtp_mock = MagicMock()
 
     def test_get(self, client):
         'Логин не отвечает на GET-запросы'
         resp = client.simulate_get(self.URI)
         assert resp.status == falcon.HTTP_405
 
-    def test_empty_post(self, client):
+    def test_lacking_data(self, client):
         'При пустом POST-запросе возвращается 400 Bad Request'
         resp = client.simulate_post(self.URI)
         assert resp.status == falcon.HTTP_400
 
     @pytest.mark.parametrize("input_json", [
-        {"email": "bad@mail.com", "password": REGISTERED_USER_PASS},
-        {"email": REGISTERED_USER_EMAIL, "password": 'bad'}
+        {"email": "bad@mail.com"},
+        {"email": REGISTERED_USER_EMAIL}
     ])
     def test_bad_data(self, client, oneUserDB, input_json):
-        'При вводе неверного эмэйла или пароля возвращается 400 Bad Request'
+        'При вводе неверного эмэйла возвращается 400 Bad Request'
         resp = client.simulate_post(self.URI, json = input_json)
         assert resp.status == falcon.HTTP_400
 
+    @patch('cyberdas.services.TransactionMail.send', new = smtp_mock)
+    def test_post(self, client, defaultDB):
+        '''
+        Эндпоинт логина должен возвращать 202 OK в случае успеха, а так же
+        отправлять письмо с валидным токеном.
+
+        Отправка письма проверяется подменой метода send из Mail и проверкой
+        того, что он вызывался с нашим USER_EMAIL. Сам модуль отправки
+        тестируется в test_mail.
+        '''
+        json = {"email": REGISTERED_USER_EMAIL}
+        resp = client.simulate_post(self.URI, json = json)
+        assert resp.status == falcon.HTTP_202
+        self.smtp_mock.assert_called_once_with(ANY, REGISTERED_USER_EMAIL, json)
+
+    @patch('cyberdas.services.TransactionMail.send', new = smtp_mock)
+    def test_post_unexisting(self, client, defaultDB):
+        '''
+        Эндпоинт логина посылает письмо, даже если пользователь не существует.
+        '''
+        json = {"email": USER_EMAIL}
+        resp = client.simulate_post(self.URI, json = json)
+        assert resp.status == falcon.HTTP_202
+        self.smtp_mock.assert_called_once_with(ANY, USER_EMAIL, json)
+
+    def test_session_not_created(self, dbses):
+        'Сессия в БД не создается до подтверждения с почты'
+        ses = dbses.query(Session).filter_by(uid = 1).first()
+        assert ses is None
+
+
+class TestValidate:
+
+    URI = '/login/validate'
+
     @pytest.fixture(scope = 'class')
-    def valid_post(self, client, oneUserDB):
-        resp = client.simulate_post(self.URI,
-                                    json = {'email': REGISTERED_USER_EMAIL,
-                                            'password': REGISTERED_USER_PASS}
-                                    )
+    def token(self, mail):
+        mail = TransactionMail(cfg, 'signup')
+        json = {"email": USER_EMAIL}
+        yield mail.generate_token(json)
+
+    def test_token_validation(self, client):
+        'При отправке невалидного токена возвращается HTTP 400'
+        resp = client.simulate_get(self.URI, params = {'token': '123'})
+        assert resp.status == falcon.HTTP_400
+
+    def test_post_unregistered(self, client, token):
+        'При отправке токена с незарегистрированным эмэйлом возвращается HTTP 400' # noqa
+        resp = client.simulate_get(self.URI, params = {'token': token})
+        assert resp.status == falcon.HTTP_400
+
+    @pytest.fixture(scope = 'class')
+    def reg_token(self, mail):
+        mail = TransactionMail(cfg, 'signup')
+        json = {"email": REGISTERED_USER_EMAIL}
+        yield mail.generate_token(json)
+
+    @pytest.fixture(scope = 'class')
+    def valid_post(self, client, reg_token, defaultDB):
+        resp = client.simulate_get(self.URI, params = {'token': reg_token})
         yield resp
 
     def test_post(self, valid_post):
-        'Эндпоинт логина должен возвращать 200 OK в случае успеха'
-        assert valid_post.status == falcon.HTTP_200
+        'При отправке верного токена эндпоинт возвращает HTTP 201'
+        assert valid_post.status == falcon.HTTP_201
 
     def test_cookie(self, valid_post):
         'В ответе на запрос должен присутствовать Set-Cookie с SESSIONID'
@@ -57,6 +118,12 @@ class TestLogin:
     def session_cookie(self, valid_post):
         yield valid_post.cookies['SESSIONID']
 
+    def test_ses_created(self, valid_post, dbses, session_cookie):
+        'При отправке верного токена на эндпоинт создается сессия'
+        ses = dbses.query(Session).filter_by(uid = 1).first()
+        assert ses is not None
+        assert ses.sid == session_cookie.value
+
     def test_cookie_attributes(self, session_cookie):
         'Куки должен иметь параметры Secure, HttpOnly и SameSite (не покрыто)'
         assert session_cookie.secure is True
@@ -66,20 +133,9 @@ class TestLogin:
         'Время жизни куки должно быть таким же, как и в конфигурации проекта'
         assert session_cookie.max_age == SES_LENGTH
 
-    def test_session_created(self, session_cookie, oneUserDB):
-        'При успешном логине должна создаваться сессия в БД'
-        with oneUserDB.session as dbses:
-            session = dbses.query(Session).filter_by(uid = 1).first()
-            assert session is not None
-            assert session.sid == session_cookie.value
-
-    def test_two_sessions(self, client, oneUserDB):
-        'Если пользователь уже залогинен, то /login создает вторую сессию'
-        resp = client.simulate_post(self.URI,
-                                    json = {'email': REGISTERED_USER_EMAIL,
-                                            'password': REGISTERED_USER_PASS}
-                                    )
-        assert resp.status == falcon.HTTP_200
-        with oneUserDB.session as dbses:
-            sessions = dbses.query(Session).filter_by(uid = 1).all()
-            assert len(sessions) == 2
+    def test_two_sessions(self, client, reg_token, dbses):
+        'Если пользователь уже залогинен, то создается вторая сессия'
+        resp = client.simulate_get(self.URI, params = {'token': reg_token})
+        assert resp.status == falcon.HTTP_201
+        sessions = dbses.query(Session).filter_by(uid = 1).all()
+        assert len(sessions) == 2
--- tests/auth/test_signup.py
+++ tests/auth/test_signup.py
@@ -1,19 +1,21 @@
 import pytest
 from unittest.mock import ANY, MagicMock, patch
+from os import environ
 
 import falcon
 
 from cyberdas.models import User
+from cyberdas.services import TransactionMail
+from cyberdas.config import get_cfg
 
-from conftest import (
-    USER_EMAIL,
-    USER_PASS,
-    FACULTY_NAME,
-    REGISTERED_USER_EMAIL,
-)
+USER_EMAIL = 'haha@mail.com'
+FACULTY_NAME = environ['FACULTY_NAME']
+REGISTERED_USER_EMAIL = environ['REGISTERED_USER_EMAIL']
+
+cfg = get_cfg()
 
 
-class TestSignup:
+class TestSender:
 
     URI = '/signup'
     smtp_mock = MagicMock()
@@ -27,30 +29,29 @@ class TestSignup:
         {},
         {"email": USER_EMAIL, "name": "Иван"},
         {"name": "Иван", "surname": "Иванов", "patronymic": "Иванович"},
-        {"email": USER_EMAIL, "password": USER_PASS, "faculty": FACULTY_NAME},
-        {"email": USER_EMAIL, "password": USER_PASS, "faculty": FACULTY_NAME,
+        {"email": USER_EMAIL, "faculty": FACULTY_NAME},
+        {"email": USER_EMAIL, "faculty": FACULTY_NAME,
          "name": "Иван", "patronymic": "Иванович"}
     ])
-    def test_lacking_data_post(self, client, input_json):
+    def test_lacking_data(self, client, input_json):
         'При POST-запросе с недостаточными данными возвращается 400 Bad Request'
         resp = client.simulate_post(self.URI, json = input_json)
         assert resp.status == falcon.HTTP_400
 
     @pytest.mark.parametrize("input_json", [
-        {"email": "badmail", "password": USER_PASS, "faculty": FACULTY_NAME,
+        {"email": "badmail", "faculty": FACULTY_NAME,
          "name": "Иван", "surname": "Иванов"},
-        {"email": USER_EMAIL, "password": 'bad', "faculty": FACULTY_NAME,
+        {"email": USER_EMAIL, "faculty": FACULTY_NAME,
          "name": "Иван", "surname": "Иванов"},
-        {"email": USER_EMAIL, "password": USER_PASS, "faculty": "bad",
+        {"email": USER_EMAIL, "faculty": "bad",
          "name": "Иван", "surname": "Иванов"},
-        {"email": REGISTERED_USER_EMAIL, "password": USER_PASS,
-         "faculty": FACULTY_NAME, "name": "Иван", "surname": "Иванов"},
+        {"email": REGISTERED_USER_EMAIL, "faculty": FACULTY_NAME,
+         "name": "Иван", "surname": "Иванов"},
     ])
-    def test_bad_data_post(self, client, input_json, oneUserDB):
+    def test_bad_data(self, client, input_json, defaultDB):
         '''
         Если пользователь ввел:
         - некорректный адрес почты
-        - слабый пароль
         - несуществующий факультет
         - уже зарегистрированную почту
         то, возвращается HTTP 400
@@ -58,28 +59,59 @@ class TestSignup:
         resp = client.simulate_post(self.URI, json = input_json)
         assert resp.status == falcon.HTTP_400
 
-    @patch('cyberdas.services.SignupMail.send_verification', new = smtp_mock)
-    def test_post(self, client, oneUserDB):
+    @patch('cyberdas.services.TransactionMail.send', new = smtp_mock)
+    def test_post(self, client, defaultDB):
         '''
-        Эндпоинт регистрации должен возвращать 200 OK в случае успеха, а так же
+        Эндпоинт регистрации должен возвращать 202 OK в случае успеха, а так же
         отправлять письмо с валидным токеном.
 
-        Отправка письма проверяется подменой метода send_verification из
-        SignupMail и проверкой того, что он вызывался с нашим USER_EMAIL.
-        Сам модуль отправки тестируется в test_mail.
+        Отправка письма проверяется подменой метода send из Mail и проверкой
+        того, что он вызывался с нашим USER_EMAIL. Сам модуль отправки
+        тестируется в test_mail.
         '''
-        resp = client.simulate_post(
-            self.URI,
-            json = {
-                "email": USER_EMAIL, "password": USER_PASS,
-                "faculty": FACULTY_NAME, "name": "Иван", "surname": "Иванов"
-            }
-        )
-        assert resp.status == falcon.HTTP_200
-        self.smtp_mock.assert_called_once_with(ANY, USER_EMAIL)
+        json = {"email": USER_EMAIL, "faculty": FACULTY_NAME,
+                "name": "Иван", "surname": "Иванов"}
+        resp = client.simulate_post(self.URI, json = json)
+        assert resp.status == falcon.HTTP_202
+        self.smtp_mock.assert_called_once_with(ANY, USER_EMAIL, json)
 
-    def test_user_added(self, oneUserDB):
-        'После регистрации пользователь должен попасть в БД'
-        with oneUserDB.session as dbses:
-            user = dbses.query(User).filter_by(email = USER_EMAIL).first()
-            assert user is not None
+    def test_user_not_created(self, dbses):
+        'Пользователь в БД не создается до подтверждения с почты'
+        user = dbses.query(User).filter_by(email = USER_EMAIL).first()
+        assert user is None
+
+
+class TestValidate:
+
+    URI = '/signup/validate'
+
+    @pytest.fixture(scope = 'class')
+    def token(self, mail):
+        mail = TransactionMail(cfg, 'signup')
+        json = {"email": USER_EMAIL, "faculty": FACULTY_NAME,
+                "name": "Иван", "surname": "Иванов"}
+        yield mail.generate_token(json)
+
+    @pytest.fixture(scope = 'class')
+    def reg_token(self, mail):
+        mail = TransactionMail(cfg, 'signup')
+        json = {"email": REGISTERED_USER_EMAIL, "faculty": FACULTY_NAME,
+                "name": "Иван", "surname": "Иванов"}
+        yield mail.generate_token(json)
+
+    def test_post(self, client, token, dbses):
+        'При отправке верного токена на эндпоинт создается пользователь'
+        resp = client.simulate_get(self.URI, params = {'token': token})
+        assert resp.status == falcon.HTTP_201
+        user = dbses.query(User).filter_by(email = USER_EMAIL).first()
+        assert user is not None
+
+    def test_token_validation(self, client):
+        'При отправке невалидного токена возвращается HTTP 400'
+        resp = client.simulate_get(self.URI, params = {'token': '123'})
+        assert resp.status == falcon.HTTP_400
+
+    def test_user_not_replaced(self, client, reg_token):
+        'Уже зарегистрированным пользователям возвращается HTTP 403'
+        resp = client.simulate_get(self.URI, params = {'token': reg_token})
+        assert resp.status == falcon.HTTP_403
