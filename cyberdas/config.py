41794d0927bae57c7d264ed25e46066bc0f589cb
qqpayne <ivanakostelov@gmail.com>
2021-09-06 20:37:45 +0300

    Абсолютный путь до конфига

--- cyberdas/config.py
+++ cyberdas/config.py
@@ -1,6 +1,8 @@
 import configparser
+import pathlib
 
 CONFIG_NAME = 'cfg.ini'
+CONFIG_PATH = pathlib.Path(__file__).parent.parent.resolve().joinpath(CONFIG_NAME) # noqa
 
 
 def get_cfg():
@@ -8,5 +10,5 @@ def get_cfg():
     Возвращает конфигурацию проекта
     '''
     config = configparser.ConfigParser(interpolation = None)
-    config.read(CONFIG_NAME)
+    config.read(CONFIG_PATH)
     return config
