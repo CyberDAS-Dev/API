20a76b8dd7740920515137d37d86cf1695d907d6
qqpayne <ivanakostelov@gmail.com>
2021-09-19 05:33:25 +0300

    Кастомная ошибка `HTTP 442`

--- cyberdas/exceptions.py
+++ cyberdas/exceptions.py
@@ -1,3 +1,6 @@
+from falcon.http_error import HTTPError
+
+
 class SessionError(Exception):
     pass
 
@@ -8,3 +11,13 @@ class NoSessionError(SessionError):
 
 class BadAuthError(SessionError):
     pass
+
+
+class HTTPNotEnoughPersonalData(HTTPError):
+    def __init__(self, fields: list = [], headers=None, **kwargs):
+        super().__init__(
+            '442 Not Enough Personal Data',
+            description=','.join(fields),
+            headers=headers,
+            **kwargs
+        )
